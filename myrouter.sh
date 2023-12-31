b19f1fbb7b1b73adcbbaaf36f5b2f32d7b3eee9e
qy2009 <51539377+qy2009@users.noreply.github.com>
2023-12-30 21:40:51 -0800

    Update myrouter.sh

--- myrouter.sh
+++ myrouter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 opkg update
-opkg install luci-compat luci-app-frpc openssh-sftp-server luci-app-argon-config
+opkg install luci-compat luci-app-frpc openssh-sftp-server luci-app-argon-config luci-app-acme
 wget --no-check-certificate -O 01_taskd_1.0.3-1_all.ipk https://raw.githubusercontent.com/qy2009/iptv//main/01_taskd_1.0.3-1_all.ipk
 wget --no-check-certificate -O 02_luci-lib-xterm_4.18.0_all.ipk https://raw.githubusercontent.com/qy2009/iptv//main/02_luci-lib-xterm_4.18.0_all.ipk
 wget --no-check-certificate -O 03_luci-lib-taskd_1.0.17_all.ipk https://raw.githubusercontent.com/qy2009/iptv//main/03_luci-lib-taskd_1.0.17_all.ipk
