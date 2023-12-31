b4cab86bb08b420a12f6a53fc1b8e026792a07a3
qy2009 <51539377+qy2009@users.noreply.github.com>
2023-12-30 23:11:36 -0800

    Update myrouter.sh

--- myrouter.sh
+++ myrouter.sh
@@ -5,9 +5,11 @@ wget --no-check-certificate -O 01_taskd_1.0.3-1_all.ipk https://raw.githubuserco
 wget --no-check-certificate -O 02_luci-lib-xterm_4.18.0_all.ipk https://raw.githubusercontent.com/qy2009/iptv//main/02_luci-lib-xterm_4.18.0_all.ipk
 wget --no-check-certificate -O 03_luci-lib-taskd_1.0.17_all.ipk https://raw.githubusercontent.com/qy2009/iptv//main/03_luci-lib-taskd_1.0.17_all.ipk
 wget --no-check-certificate -O 04_luci-app-store_0.1.14-2_all.ipk https://raw.githubusercontent.com/qy2009/iptv//main/04_luci-app-store_0.1.14-2_all.ipk
+wget --no-check-certificate -O OpenClash+Kernel_a53.run https://raw.githubusercontent.com/qy2009/iptv//main/OpenClash+Kernel_a53.run
 opkg install 01_taskd_1.0.3-1_all.ipk
 opkg install 02_luci-lib-xterm_4.18.0_all.ipk
 opkg install 03_luci-lib-taskd_1.0.17_all.ipk 
 opkg install 04_luci-app-store_0.1.14-2_all.ipk
 uci set luci.main.mediaurlbase='/luci-static/argon'
 uci commit
+sh OpenClash+Kernel_a53.run
