64e05eeb9efc990ff952b1758dbebfc363acb6d8
qy2009 <51539377+qy2009@users.noreply.github.com>
2025-04-01 07:06:23 -0700

    Update myrouter.sh

--- myrouter.sh
+++ myrouter.sh
@@ -5,9 +5,9 @@ wget --no-check-certificate -O 01_taskd_1.0.3-1_all.ipk https://cdn.jsdelivr.net
 wget --no-check-certificate -O 02_luci-lib-xterm_4.18.0_all.ipk https://cdn.jsdelivr.net/gh/qy2009/iptv@master/02_luci-lib-xterm_4.18.0_all.ipk
 wget --no-check-certificate -O 03_luci-lib-taskd_1.0.17_all.ipk https://cdn.jsdelivr.net/gh/qy2009/iptv@master/03_luci-lib-taskd_1.0.17_all.ipk
 wget --no-check-certificate -O 04_luci-app-store_0.1.14-2_all.ipk https://cdn.jsdelivr.net/gh/qy2009/iptv@master/04_luci-app-store_0.1.14-2_all.ipk
-wget --no-check-certificate -O OpenClash+Kernel_a53.run https://cdn.jsdelivr.net/gh/qy2009/iptv@master/OpenClash+Kernel_a53.run
+wget --no-check-certificate -O OpenClash+Kernel_a53.run https://cdn.jsdelivr.net/gh/qy2009/iptv@master/OpenClash_0.46.014+aarch_64_core.run
 opkg install 01_taskd_1.0.3-1_all.ipk
 opkg install 02_luci-lib-xterm_4.18.0_all.ipk
 opkg install 03_luci-lib-taskd_1.0.17_all.ipk 
 opkg install 04_luci-app-store_0.1.14-2_all.ipk
-sh OpenClash+Kernel_a53.run
+sh OpenClash_0.46.014+aarch_64_core.run
