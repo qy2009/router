702f1dc8c931e08a2f3efcfcf70943c5c60db709
qy2009 <51539377+qy2009@users.noreply.github.com>
2026-05-14 06:35:39 -0700

    Update and rename myrouter.sh to homerouter.sh

--- homerouter.sh
+++ homerouter.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 opkg update
-opkg install nano luci-compat luci-app-frpc openssh-sftp-server luci-app-p910nd luci-app-lucky luci-theme-argon
+opkg install zram-swap nano luci-compat luci-app-frpc openssh-sftp-server luci-app-p910nd luci-app-lucky luci-theme-argon
 wget --no-check-certificate -O 01_taskd.ipk https://cdn.jsdelivr.net/gh/qy2009/iptv@master/taskd_1.0.3-2_all.ipk
 wget --no-check-certificate -O 02_luci-lib-xterm.ipk https://cdn.jsdelivr.net/gh/qy2009/iptv@master/02_luci-lib-xterm_4.18.0_all.ipk
 wget --no-check-certificate -O 03_luci-lib-taskd.ipk https://cdn.jsdelivr.net/gh/qy2009/iptv@master/luci-lib-taskd_1.0.25_all.ipk
